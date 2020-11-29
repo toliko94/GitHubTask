b734a81e4655f719247b5106ecf05719ea96c0c9
Adel Keinan <adelke@ac.sce.ac.il>
2020-11-29 23:14:13 +0200

    volenteer updateed

--- volenteer.js
+++ volenteer.js
@@ -1,18 +1,34 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  // var firebaseConfig = {
+  //   apiKey: "AIzaSyDDyo3rtSkUIA5L9ixFqs13TulEnIPM3UI",
+  //   authDomain: "wepo4u-project-22.firebaseapp.com",
+  //   databaseURL: "https://wepo4u-project-22.firebaseio.com",
+  //   projectId: "wepo4u-project-22",
+  //   storageBucket: "wepo4u-project-22.appspot.com",
+  //   messagingSenderId: "859760012059",
+  //   appId: "1:859760012059:web:0aff433aea5b8dac1801be",
+  //   measurementId: "G-CYS41V24S2"
+  // };
+  // // Initialize Firebase
+  // firebase.initializeApp(firebaseConfig);
+  // //firebase.analytics();
+  // const auth = firebase.auth();
 
+  
+        
+  
 
+  
+        
 
+  // function signUp(){
 
-<script>
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyBlrW83Q55ZRpHhytACmaryrIJunaRhNd4",
-    authDomain: "wepo4u-f930a.firebaseapp.com",
-    databaseURL: "https://wepo4u-f930a.firebaseio.com",
-    projectId: "wepo4u-f930a",
-    storageBucket: "wepo4u-f930a.appspot.com",
-    messagingSenderId: "1028322060202",
-    appId: "1:1028322060202:web:1ac00b638fef4a1ead2dd3"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-</script>
+  //   // var email = document.getElementById("email");
+  //   // var password = document.getElementById("password");
+
+  //   // const promise = auth.createUserwithEmailAndPassword(email.nodeValue, password);
+  //   // promise.catch(e => alert(e.message));
+    
+  //   alert("Signed In");
+  // }
