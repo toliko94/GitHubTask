116af692a18e4f4db4f60c6aad7f6a3b5cb26f0c
Adel Keinan <adelke@ac.sce.ac.il>
2020-11-29 18:03:06 +0200

    volenteer html page and js file

---
+++ volenteer.js
@@ -0,0 +1,18 @@
+
+
+
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBlrW83Q55ZRpHhytACmaryrIJunaRhNd4",
+    authDomain: "wepo4u-f930a.firebaseapp.com",
+    databaseURL: "https://wepo4u-f930a.firebaseio.com",
+    projectId: "wepo4u-f930a",
+    storageBucket: "wepo4u-f930a.appspot.com",
+    messagingSenderId: "1028322060202",
+    appId: "1:1028322060202:web:1ac00b638fef4a1ead2dd3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
