2f06d73f375712ece786400a5328e8b56b69b4da
toliko <73425091+toliko94@users.noreply.github.com>
2020-11-30 13:55:05 +0200

    Old man pages + Js file

--- Oldmanjs.js
+++ Oldmanjs.js
@@ -18,6 +18,8 @@
 
   const auth = firebase.auth();
 
+  //SingUp - Function
+
     function singUp()
     {
        
@@ -26,7 +28,42 @@
 
     const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
     promise.catch(e => alert(e.message));
-        alert("Signed Up");
+        alert("תודה שהצטרפת :)");
     }
   
   
+    //SingIn - Function
+
+    function signIn()
+    {
+      var email = document.getElementById("email");
+      var password = document.getElementById("password");
+
+       const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+       promise.catch(e => alert(e.message));
+
+        alert("התחברת !" + email.value );
+        
+    }
+
+    function signOut()
+    {
+      auth.signOut();
+      alert("התנתקת !")
+      location.replace("Oldman.html")
+    }
+
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        // User is signed in.
+        
+        alert("משתמש מחובר :" + email.value)
+        location.replace("OldmanEnter.html")
+      } else {
+        // No user is signed in.
+        alert("משתמש לא מחובר !")
+        
+      }
+    });
+
+    
